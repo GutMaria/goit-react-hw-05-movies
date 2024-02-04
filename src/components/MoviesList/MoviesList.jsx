c4ee6d442de1ad4e4ea77a352ffad1cb3a645d14
GutMaria <gootmaria@gmail.com>
2024-02-04 13:42:48 +0200

    add GoBackLink

--- src/components/MoviesList/MoviesList.jsx
+++ src/components/MoviesList/MoviesList.jsx
@@ -1,20 +1,26 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import css from './movies-list.module.css';
 import defaultPoster from '../../image/defaultPoster.jpg'
 
-const MoviesList = ({items}) => {
+const MoviesList = ({ items }) => {
+  
+// локація для кнопки Назад на сторінці ДеталіКіно
+  const location = useLocation();
+
+
   const elements = items.map(({ id, title, poster_path }) => {
     const imgUrl = poster_path ? ('https://image.tmdb.org/t/p/w500' + poster_path): defaultPoster;
-
   return (
     <li key={id} className={css.movieItem}>
-      <Link to={`/movies/${id}`} className={css.link}>
+      <Link to={`/movies/${id}`} state={{ from: location }} className={css.link}>
         <img src={imgUrl} alt={title} width='300' />
         <h3 className={css.movieTitle}>{title}</h3>
       </Link>
   </li>
   )
-})
+  })
+  
+  
   
   return <ul className={css.moviesList}>{elements}</ul>
 }
--- src/pages/MovieDetailsPage/MovieDetailsPage.jsx
+++ src/pages/MovieDetailsPage/MovieDetailsPage.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { useParams, Outlet, Link } from "react-router-dom";
+import { useParams, Outlet, Link , useLocation } from "react-router-dom";
 import {getMovieDetails} from '../../apiServices/apiServices'
 import css from './movie-details-page.module.css';
 
@@ -29,14 +29,20 @@ const MovieDetailsPage = () => {
   }, [id]);
   
 const genreList = movie.genres?.map((genre) => <li key={genre.id} className={css.text}>{genre.name}</li>);
-
-
   const image = 'https://image.tmdb.org/t/p/w500' + movie.poster_path;
+
+  // Для кнопки повернення назад:
+  const location = useLocation();
+    const backLinkHref = location.state?.from ?? "/";
+
+  // -----------------------------------
 
 
   return (<>
     {loading && <p>...Loading</p>}
     {error && <p>Упс... щось пішло не так, спробуйте ще раз!</p>}
+    
+    <Link to={backLinkHref} className={css.goBackLink}>GO BACK</Link>
     {movie && (<>
     <div className={css.wrapper}>
         <img src={image} alt={movie.title} />
@@ -53,10 +59,10 @@ const genreList = movie.genres?.map((genre) => <li key={genre.id} className={css
         <h4>Additional information</h4>
         <ul>
           <li>
-            <Link to='cast'>Cast</Link>
+            <Link to='cast' state={{from: backLinkHref}}>Cast</Link>
           </li>
           <li>
-            <Link to='reviews'>Reviews</Link>
+            <Link to='reviews' state={{from: backLinkHref}}>Reviews</Link>
           </li>
         </ul>
       </div>
